93b9e1de6cfe0188f1306b8b93cc91246e4f55b8
Vallu <vallu9@hotmail.com>
2025-08-31 21:33:04 +0300

    fix: fill diagonals straight from main flat global matrix

--- 1D Heat Diffusion Wire/main.cu
+++ 1D Heat Diffusion Wire/main.cu
@@ -5,17 +5,17 @@
 #include <cuda_runtime.h>
 
 // Kernel: build element matrices
-__global__ void create_element(float *out, float length, float k, int Ne, int N) {
-    int e = threadIdx.x;
-    if (e >= N) return;
+__global__ void create_element(float *element_matrices, float length, float k, int Ne, int N) {
+	int e = threadIdx.x;
+	if (e >= N) return;
 
-    float l = length / N;
+	float l = length / N;
 
-    // Local 2x2 matrix
-    out[e*Ne*Ne + 0*Ne + 0] = k / l;
-    out[e*Ne*Ne + 0*Ne + 1] = -k / l;
-    out[e*Ne*Ne + 1*Ne + 0] = -k / l;
-    out[e*Ne*Ne + 1*Ne + 1] = k / l;
+	// Local 2x2 matrix
+	element_matrices[e*Ne*Ne + 0*Ne + 0] = k / l;
+	element_matrices[e*Ne*Ne + 0*Ne + 1] = -k / l;
+	element_matrices[e*Ne*Ne + 1*Ne + 0] = -k / l;
+	element_matrices[e*Ne*Ne + 1*Ne + 1] = k / l;
 }
 
 // Thomas algorithm solver for tridiagonal system
@@ -25,47 +25,32 @@ void thomas(int n, float *c, float *d, float *e, float *b, float *x) {
 	// forward sweep
 	for (int i = 1; i < n; ++i)
 	{
-		float m = c[i] / d[i-1];
-		d[i] -= m * e[i-1];
-		b[i] -= m * b[i-1];
+		float m = c[i] / d[i - 1];
+		d[i] -= m * e[i - 1];
+		b[i] -= m * b[i - 1];
 	}
 
 	// back substitution
-	x[n-1] = b[n-1] / d[n-1];
+	x[n - 1] = b[n - 1] / d[n - 1];
 
-	for (int i = n-2; i >= 0; --i)
-		x[i] = (b[i] - e[i]*x[i+1]) / d[i];
+	for (int i = n - 2; i >= 0; --i)
+		x[i] = (b[i] - e[i] * x[i +1 ]) / d[i];
 }
 
 int main() {
-	int N = 5;			// number of elements
+	int N = 11;			// number of elements
 	int Ne = 2;			// nodes per element
 	int nodes = N + 1;
 	float length = 1.0f;
 	float k = 1.0f;
-	float T_first = 60.0f, T_last = -54.0f;		//boundary conditions
+	float T_first = 100.0f, T_last = 60.0f;		//boundary conditions
 
 	// --- Create element matrices on device
-	float *out;
-	cudaMallocManaged(&out, N * Ne * Ne * sizeof(float));
+	float *element_matrices;
+	cudaMallocManaged(&element_matrices, N * Ne * Ne * sizeof(float));
 
-	create_element<<<1, N>>>(out, length, k, Ne, N);
+	create_element<<<1, N>>>(element_matrices, length, k, Ne, N);
 	cudaDeviceSynchronize();
-
-	float K[N + 1][N + 1] = {0}; // for N=5 → 6 nodes
-	for (int e = 0; e < N; e++) {
-		int n1 = e;
-		int n2 = e + 1;
-		float k11 = out[e*Ne*Ne + 0];
-		float k12 = out[e*Ne*Ne + 1];
-		float k21 = out[e*Ne*Ne + 2];
-		float k22 = out[e*Ne*Ne + 3];
-
-		K[n1][n1] += k11;
-		K[n1][n2] += k12;
-		K[n2][n1] += k21;
-		K[n2][n2] += k22;
-	}
 
 	// Reduced system size = (nodes-2)
 	int nRed = nodes - 2;
@@ -75,38 +60,33 @@ int main() {
 			b[nRed] = {0},
 			x[nRed] = {0};
 
-	// Fill tridiagonal arrays from reduced system
+
+	// Fill diagonals for Thomas algorithm //this task could be given to kernel
 	for (int i = 0; i < nRed; ++i)
 	{
-		d[i] = K[i + 1][i + 1];			// main diagonal
+		int node = i + 1;
+
+		int eL = node - 1;
+		int eR = node;
+
+		int baseL = eL * Ne * Ne;
+		int baseR = eR * Ne * Ne;
+
+		d[i] = element_matrices[baseL + 3] + element_matrices[baseR];
 		if (i > 0)
-			c[i] = K[i + 1][i];			// sub-diagonal
-		if (i < nRed-1)
-			e[i] = K[i + 1][i + 2];		// super-diagonal
+			c[i] = element_matrices[baseL + 2];
+		if (i < nRed - 1)
+			e[i] = element_matrices[baseL + 1];
+		c[0] = 0;
+		e[nRed - 1] = 0;
 
 		if (i == 0)
-			b[i] = -K[1][0] * T_first;
-		else if (i == nRed-1)
-			b[i] = -K[nodes - 2][nodes - 1] * T_last;
+			b[i] = -element_matrices[baseL + 2] * T_first;
+		else if (i == nRed - 1)
+			b[i] = -element_matrices[baseR + 1] * T_last;
 		else
 			b[i] = 0;
 	}
-
-	for (int i = 0; i < nRed; ++i)
-	{
-		printf("d[%i] = %f, ", i, d[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < nRed; ++i)
-	{
-		printf("c[%i] = %f, ", i, c[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < nRed; ++i)
-	{
-		printf("e[%i] = %f, ", i, e[i]);
-	}
-	printf("\n");
 
 	thomas(nRed, c, d, e, b, x);
 
@@ -123,6 +103,21 @@ int main() {
 		printf("T[%d] = %.4f\n", i+1, T[i]);
 	}
 
-	cudaFree(out);
+	cudaFree(element_matrices);
 	return 0;
 }
+
+	// printf("nRed = %i\n", nRed);
+
+	// for (int i = 0; i < nRed; ++i)
+	// 	printf("d[%i] = %f, ", i, d[i]);
+	// printf("\n");
+	// for (int i = 0; i < nRed; ++i)
+	// 	printf("c[%i] = %f, ", i, c[i]);
+	// printf("\n");
+	// for (int i = 0; i < nRed; ++i)
+	// 	printf("e[%i] = %f, ", i, e[i]);
+	// printf("\n");
+	// for (int i = 0; i < nRed; ++i)
+	// 	printf("b[%i] = %f, ", i, b[i]);
+	// printf("\n\n");
